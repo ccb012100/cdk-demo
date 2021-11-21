4c48def2743d762f96b41f71bf36cccbd37cf50f
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-11-21 15:56:30 -0600

    cdk demo TypeScript app

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,15 +1,26 @@
 import * as cdk from '@aws-cdk/core';
 // import * as sqs from '@aws-cdk/aws-sqs';
+import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'MainVpc', {
+      // number of Availability Zones (AZs) to use
+      maxAzs: 2,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      // define a single subnet configuration per AZ
+      subnetConfiguration: [
+        {
+          // subnet mask 255.255.255.0
+          cidrMask: 24,
+          // name for each of these subnets
+          name: 'public-subnet',
+          // subnet type to be used
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+    });
   }
 }
